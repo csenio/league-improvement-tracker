679f5608ac920f9f3a8632ff054253b563ede140
Jesco Wuester <jescowuester@gmail.com>
2019-01-02 17:31:41 +0100

    rudimentary backed

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,22 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var axios = require("axios");
+var config = require("../config.js");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/u/:region/:user", (req, res, next) => {
+  console.log(config.key);
+  axios
+    .get(
+      `https://${
+        req.params.region
+      }.api.riotgames.com/lol/summoner/v4/summoners/by-name/${
+        req.params.user
+      }?api_key=${config.key}`
+    )
+    .then(res => console.log("success", res))
+    .catch(err => console.log("error", err));
+  // debugger;
+  res.send(`respond with ${req.params.user} data`);
 });
 
 module.exports = router;
