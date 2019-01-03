6642c5cd23fff062c2beff9f64d56d7c046dfd41
Jesco Wuester <jescowuester@gmail.com>
2019-01-03 18:32:20 +0100

    reached datadragon api

--- routes/users.js
+++ routes/users.js
@@ -2,21 +2,76 @@ var express = require("express");
 var router = express.Router();
 var axios = require("axios");
 var config = require("../config.js");
+// var champions = require("../champion.json");
+var champions = undefined;
+var errors = [];
 
-router.get("/u/:region/:user", (req, res, next) => {
-  console.log(config.key);
+setChampionObject("8.24.1");
+
+function setChampionObject(version) {
   axios
     .get(
-      `https://${
-        req.params.region
-      }.api.riotgames.com/lol/summoner/v4/summoners/by-name/${
-        req.params.user
-      }?api_key=${config.key}`
+      `https://ddragon.leagueoflegends.com/cdn/${version}/data/en_US/champion.json`
     )
-    .then(res => console.log("success", res))
-    .catch(err => console.log("error", err));
-  // debugger;
-  res.send(`respond with ${req.params.user} data`);
+    .then(res => (champions = res))
+    .catch(err => catchFetchingError("error fetching all champions", err));
+}
+
+function catchFetchingError(description, errorMessage) {
+  errors.push({ description: description, errorMessage: errorMessage });
+}
+
+function getUserId(username, region, cb) {
+  axios
+    .get(
+      `https://${region}.api.riotgames.com/lol/summoner/v4/summoners/by-name/${username}?api_key=${
+        config.key
+      }`
+    )
+    .then(res => cb(res))
+    .catch(err => cb(null, err));
+}
+
+// getUserId("cubefox", "euw1", (result, err) => {
+//   if (err) {
+//     console.log("error", err);
+//     debugger;
+//   } else {
+//     return result.accountId;
+//   }
+// });
+
+// getUserMatchlist("euw", (res, err) => {
+//   console.log(res);
+//   console.log(err);
+// });
+
+getChampionId("region", res => {
+  console.log(res);
 });
+
+function getUserMatchlist(region, cb) {
+  //end < 100
+
+  axios
+    .get(
+      `https://${region}.api.riotgames.com/lol/static-data/v3/tarball-links?api_key=${
+        config.key
+      }`
+    )
+    .then(res => cb(res))
+    .catch(err => cb(null, err));
+}
+
+function getChampionId(region, cb) {
+  //end < 100
+
+  axios
+    .get(
+      `https://ddragon.leagueoflegends.com/cdn/8.24.1/data/en_US/champion.json`
+    )
+    .then(res => cb(res))
+    .catch(err => cb(null, err));
+}
 
 module.exports = router;
